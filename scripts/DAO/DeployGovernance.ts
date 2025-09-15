ddd5f014bf60bda729ee75e4f07cf655c823de1e
c4chakri <chakravarthi.l@mobiusdtaas.ai>
2025-09-15 13:58:35 +0530

    Added vote count

--- scripts/DAO/DeployGovernance.ts
+++ scripts/DAO/DeployGovernance.ts
@@ -1,11 +1,11 @@
-import { Address,toNano } from "locklift";
+import { Address, toNano } from "locklift";
 
 async function main() {
   const signer = (await locklift.keystore.getSigner("0"))!;
-  
+
 
   const chakri = new Address("0:777fa2283eea7b1364b015571c4d3649f4f501d83d24e4f8876e753fc3ab5081");
- 
+
   const { contract: sample, tx } = await locklift.factory.deployContract({
     contract: "GovernanceToken",
     publicKey: signer.publicKey,
@@ -13,13 +13,13 @@ async function main() {
       nonce_: locklift.utils.getRandomNonce(),
     },
     constructorParams: {
-       
-        treasuryAddress:new Address("0:98f6da4d2c283018b9174d2c6863d458bc925fb84a52697dc129f618a9f84e13"),
-        _deedNFT:new Address("0:63e1aac8fa36ab60ba041b09888da2eda43b4d1489480fcd9f0b2e86e0a1270a"),
-        _founderNFT:new Address("0:63e1aac8fa36ab60ba041b09888da2eda43b4d1489480fcd9f0b2e86e0a1270a"),
-        _admin:chakri,
-        _complianers:[chakri]
-    
+
+      treasuryAddress: new Address("0:98f6da4d2c283018b9174d2c6863d458bc925fb84a52697dc129f618a9f84e13"),
+      _deedNFT: new Address("0:63e1aac8fa36ab60ba041b09888da2eda43b4d1489480fcd9f0b2e86e0a1270a"),
+      _founderNFT: new Address("0:63e1aac8fa36ab60ba041b09888da2eda43b4d1489480fcd9f0b2e86e0a1270a"),
+      _admin: chakri,
+      _complianers: [chakri]
+
     },
     value: locklift.utils.toNano(1),
   });
@@ -29,32 +29,50 @@ async function main() {
   );
 
   // create a proposal 
-console.log("create a proposal",new Date().getTime());
+  console.log("create a proposal", new Date().getTime());
 
-   {
-        const { traceTree } = await locklift.tracing.trace(
-          sample.methods
-                .createProposalRequest({
-                  _title: "My First Proposal ",
-                  _description: "This is my first proposal",
-                  _vote_start: 0, 
-                  _vote_end: 10,
-                  _ptype: 2,
-                  _minVoters: 3,
-                  _minPercent: 60,
-                  amount: (0),
-                  _blockAddress: new Address("0:0000000000000000000000000000000000000000000000000000000000000000"),
+  {
+    const { traceTree } = await locklift.tracing.trace(
+      sample.methods
+        .createProposalRequest({
+          _title: "My First Proposal ",
+          _description: "This is my first proposal",
+          _vote_start: 0,
+          _vote_end: 10,
+          _ptype: 4,
+          _minVoters: 3,
+          _minPercent: 60,
+          amount: (0),
+          _blockAddress: new Address("0:7179e9aeb58d6fe97610ffcf605eded4f3a161cee84bc0d67bfdf55e6a6a6491"),
 
-                })
-                .send({
-                    from: chakri,
-                    amount: toNano(1),
-                }),
-        );
+        })
+        .send({
+          from: chakri,
+          amount: toNano(1),
+        }),
+    );
 
-        await traceTree?.beautyPrint();
-    }
-  
+    await traceTree?.beautyPrint();
+  }
+
+  // vote on the proposal
+  console.log("vote on the proposal", new Date().getTime());
+
+  {
+    const { traceTree } = await locklift.tracing.trace(
+      sample.methods
+        .voteByUser({
+          _propID: 0,
+          _supports: true,
+        })
+        .send({
+          from: chakri,
+          amount: toNano(1),
+        }),
+    );
+
+    await traceTree?.beautyPrint();
+  }
 }
 
 main()
